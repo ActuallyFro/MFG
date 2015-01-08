881d7d22e6e46f2a9abe269bd94bb4d5823fd877
ActuallyFro <ActuallyFro@github.com>
2015-01-07 20:55:20 -0500

    Ported to Linux; Created Function for Square

--- main.c
+++ main.c
@@ -1,70 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int createSquare(int consoleORchat, int x, int y, int z, char buildingMaterial[], int direction, int rotation, int pointStart, int pointStop);
+int printfShapes();
+
 int main(){
 	printf("\n\n\nMinecraft Fill Tool Building Generator");
 	printf("\n======================================");
 	int x,y,z;
-	int xStart,yStart,zStart;
-	int xStop,yStop,zStop;
 
-	int i,j,k;
-	int Length, Width, Height;
 	int buildingType=0;
 	char buildingMaterial[50];
-	int shapesTotal=2;
 	int consoleORchat=0;
-	
+
 	int MaxHeight=255;
 	int MinHeight=0;
 	int HeightOffset=0;//Imagine this as a method to skip the rendering of the first X layers
 	int HeightStop=0;//Imagine this as a method to stop the rendering at layer X
 	int Hollow=0;
 	int EdgesOnly=0;
-	
+
 	printf("\nPlease enter a Material (e.g. minecraft:brick_block:");
 	scanf("%[^\n]", buildingMaterial);
-	
+
 	printf("\n");
 	printf("\nConsole or Chat (0/1)?: ");
 	scanf("%i",&consoleORchat);
-	
+
+	int shapesTotal=9;
+	printShapes();
+
+	do{
+		printf("\n\nWhat Type of shape would you like to create? " );
+		scanf("%i",&buildingType);
+	}while(0>buildingType&&buildingType>shapesTotal);
+
+	printf("Please enter a starting <x> <y> <z>: ");
+	scanf("%i %i %i",&x,&y,&z);
+
+	if(buildingType==1){
+		createSquare(consoleORchat,x, y, z, buildingMaterial, 0, 0, 0, 0);
+	}
+	printf("\n\n");
+	return EXIT_SUCCESS;
+}
+
+
+int printShapes(){
 	printf("\n");
 	printf("\n[1] Square");
 	printf("\n[2] Rectangle");
 	printf("\n[3] Triangular Prism");
-	//printf("[4] Pyramid");
-	//printf("[5] Sphere");
-	//printf("[6] Cylinder");
-	//printf("[7] Sphere");
-	//printf("[8] Cone");
-	//printf("[9] Doughnut");
-	
-	do{
-	printf("\n\nWhat Type of shape would you like to create? " );
-	scanf("%i",&buildingType);
-	}while(0>buildingType&&buildingType>shapesTotal);
-	
-	printf("Please enter a starting <x> <y> <z>: ");
-	scanf("%i %i %i",&x,&y,&z);
-	
+	printf("\n[4] Pyramid");
+	printf("\n[5] Sphere");
+	printf("\n[6] Cylinder");
+	printf("\n[7] Sphere");
+	printf("\n[8] Cone");
+	printf("\n[9] Doughnut");
+
+	return EXIT_SUCCESS;
+}
+
+int createSquare(int consoleORchat, int x, int y, int z, char buildingMaterial[], int direction, int rotation, int pointStart, int pointStop){
+	int Length, Width, Height;
+	int xStart,yStart,zStart;
+	int xStop,yStop,zStop;
+	int i,j,k;
+
 	xStart=x;
 	yStart=y;
 	zStart=z;
 
-	if(buildingType==1){	
-		printf("\n\nPlease Enter a Length: ");
+	printf("\n\nPlease Enter a Length: ");
 		scanf("%i",&Length);
-		
+
 		if(Length>180 || Length < 1){
-			printf("[WARNING] Size %i is out of bounds (greater than 180 or less than 1) attempting to correct size...");
+			printf("[WARNING] Size %i is out of bounds (greater than 180 or less than 1) attempting to correct size...",Length);
 			Length=Length%180;
 		}
 
 		//These never change
 		xStart=x;
 		zStart=z;
-		
+
 		printf("\n\nCopy and paste this into your console/chat:");
 		for(i=0;i<Length;i++){
 			yStart=i+y;
@@ -79,8 +97,5 @@ int main(){
 				printf("\n/fill %i %i %i %i %i %i %s",xStart,yStart,zStart,xStop,yStop,zStop,buildingMaterial);
 			}
 		}
-	}
-	
-	//printf("Processing: <%i> <%i> <%i>",x,y,z);
 	return EXIT_SUCCESS;
 }
