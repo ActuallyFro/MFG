346f4bd8b63d560d593dcee5bfcb6cf2991ca31f
ActuallyFro <ActuallyFro@github.com>
2015-04-18 16:00:29 -0400

    added depth parsing; was trying to test the non-added triangular prism code

--- other/buildSphere.bat
+++ other/buildSphere.bat
@@ -1,3 +1,3 @@
-MFG.exe -O -m brick_block -nhw -x -50 -y 166 -z 100 --console -s 5 -w 30
+MFG.exe -O -m glowstone -nhw -x -50 -y 166 -z 100 --console -s 3 -w 30 -l 10
 
 %rem MFG.exe -O -m brick_block -nhw -wh 1 -x -50 -y 166 -z 100 --console -s 5 -w 30
